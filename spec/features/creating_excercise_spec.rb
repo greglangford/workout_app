01a040e308ab3fbe7056a382b2ba92f4d35ec49e
Greg Langford <greg@langford.me>
2016-06-04 15:34:27 +0100

    add validations to excercises

--- spec/features/creating_excercise_spec.rb
+++ spec/features/creating_excercise_spec.rb
@@ -23,4 +23,24 @@ RSpec.feature "Creating exercise" do
     exercise = Exercise.last
     expect(current_path).to eq(user_exercise_path(@user, exercise))
   end
+
+  scenario "with invalid input" do
+    visit "/"
+
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+
+    fill_in "Duration", with: nil
+    fill_in "Workout Details", with: ""
+    fill_in "Activity date", with: ""
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Excercise has not been created.")
+
+    expect(page).to have_content("Duration in min can't be blank")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout can't be blank")
+    expect(page).to have_content("Workout date can't be blank")
+  end
 end
